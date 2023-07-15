57ce3ae4f8a6f057546f7901f8e4c9735cdde537
CheateRYT <cheaterytyt@gmail.com>
2023-07-15 19:52:44 +0300

    Timer Added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+  //Tabs
+  
   const tabs = document.querySelectorAll(".tabheader__item"),
     tabsContent = document.querySelectorAll(".tabcontent"),
     tabParent = document.querySelector(".tabheader__items"),
@@ -35,4 +38,51 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
   });
+
+  //Timer
+
+  const deadLine = '2023-10-11';
+ function getTimeRemaining(endtime) {
+  const t = Date.parse(endtime) - Date.parse(new Date()),
+    days = Math.floor(t / (1000 * 60 * 60 * 24)),
+    hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+    minutes = Math.floor((t / (1000 * 60)) % 60),
+    seconds = Math.floor((t / 1000) % 60);
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+  function  getZero (num) {
+    if (num >=0 && num <10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+ function setClock(selector, endtime) {
+  const timer = document.querySelector(selector),
+    days = timer.querySelector('#days'),
+    hours = timer.querySelector('#hours'),
+    minutes = timer.querySelector('#minutes'),
+    seconds = timer.querySelector('#seconds');
+    updateClock();
+   function updateClock() {
+    const t = getTimeRemaining(endtime);
+     days.innerHTML = getZero(t.days);
+    hours.innerHTML = getZero(t.hours);
+    minutes.innerHTML = getZero(t.minutes);
+    seconds.innerHTML = getZero(t.seconds);
+     if (t.total <= 0) {
+      clearInterval(timeInterval);
+    }
+  }
+  const timeInterval = setInterval(updateClock, 1000);
+}
+ setClock('.timer', deadLine);
 });
