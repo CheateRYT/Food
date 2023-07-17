cc423ea6072d94a9f4c87686f89c716c3146406a
CheateRYT <cheaterytyt@gmail.com>
2023-07-17 10:50:39 +0300

    Timer fixed

--- js/script.js
+++ js/script.js
@@ -43,11 +43,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const deadLine = '2023-10-11';
  function getTimeRemaining(endtime) {
-  const t = Date.parse(endtime) - Date.parse(new Date()),
+  let days,hours, minutes, seconds;
+  const t = Date.parse(endtime) - Date.parse(new Date());
+  if (t<= 0) {
+    days = 0;
+    hours = 0
+    seconds = 0;
+    minutes = 0;
+  } else {
     days = Math.floor(t / (1000 * 60 * 60 * 24)),
     hours = Math.floor((t / (1000 * 60 * 60)) % 24),
     minutes = Math.floor((t / (1000 * 60)) % 60),
     seconds = Math.floor((t / 1000) % 60);
+  }
   return {
     'total': t,
     'days': days,
