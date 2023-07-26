ed13a713afb40a6f0faa80cbb36c392950f979b7
CheateRYT <cheaterytyt@gmail.com>
2023-07-26 12:57:45 +0300

    post method form added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 document.addEventListener("DOMContentLoaded", function () {
   //Tabs
 
@@ -161,10 +159,10 @@ document.addEventListener("DOMContentLoaded", function () {
     render() {
       const element = document.createElement("div");
       if (this.classes.length === 0) {
-        this.element = 'menu__item '
-        element.classList.add(this.element)
+        this.element = "menu__item ";
+        element.classList.add(this.element);
       } else {
-      this.classes.forEach(className => element.classList.add(className));
+        this.classes.forEach((className) => element.classList.add(className));
       }
 
       element.innerHTML = `
@@ -188,29 +186,129 @@ document.addEventListener("DOMContentLoaded", function () {
     'Меню "Фитнес"',
     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
     9,
-    '.menu .container',
-    'menu__item',
+    ".menu .container",
+    "menu__item"
   ).render();
 
   new MenuCard(
     "img/tabs/elite.jpg",
     "elite",
-    'Меню “Премиум”',
-    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    "Меню “Премиум”",
+    "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
     550,
-    '.menu .container',
-    'menu__item'
+    ".menu .container",
+    "menu__item"
   ).render();
-
 
   new MenuCard(
     "img/tabs/post.jpg",
     "post",
     'Меню "Постное"',
-    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
     430,
-    '.menu .container',
-    'menu__item'
+    ".menu .container",
+    "menu__item"
   ).render();
+
+  // Forms form-data
+
+  // const forms = document.querySelectorAll("form");
+
+  // const message = {
+  //   loading: 'Загрузка',
+  //   success : 'Спасибо! Скоро мы с вами свяжемся',
+  //   failure:  'Что-то пошло не так!'
+  // }
+
+  // forms.forEach(item => {
+  //   postData(item);
+  // })
+
+  // function postData(form) {
+  //   form.addEventListener("submit", (event) => {
+  //     event.preventDefault();
+
+  //     const statusMesage = document.createElement('div');
+  //     statusMesage.classList.add('status')
+  //     statusMesage.textContent = message.loading
+  //     form.append(statusMesage)
+
+
+  //     const request = new XMLHttpRequest();
+  //     request.open("POST", "server.php");
+
+  //    // request.setRequestHeader("Content-type", "multipart/form-data"); 
+  //    //У form-data автоматически подставляется  заголовок
+  //     const formData = new FormData(form);
+  //     request.send(formData)
+
+  //     request.addEventListener('load' , () => {
+  //       if (request.status === 200) {
+  //         console.log(request.response);
+  //         statusMesage.textContent = message.success;
+  //         form.reset();
+  //         setTimeout(() => {
+  //           statusMesage.remove();
+  //         }, 2000)
+  //       } else {
+  //         statusMesage.textContent = message.failure;
+  //       }
+  //     })
+  //   });
+  // }
+
+ //Forms JSON
+
+ const forms = document.querySelectorAll("form");
+
+ const message = {
+   loading: 'Загрузка',
+   success : 'Спасибо! Скоро мы с вами свяжемся',
+   failure:  'Что-то пошло не так!'
+ }
+
+ forms.forEach(item => {
+   postData(item);
+ })
+
+ function postData(form) {
+   form.addEventListener("submit", (event) => {
+     event.preventDefault();
+
+     const statusMesage = document.createElement('div');
+     statusMesage.classList.add('status')
+     statusMesage.textContent = message.loading
+     form.append(statusMesage)
+
+
+     const request = new XMLHttpRequest();
+     request.open("POST", "server.php");
+
+     request.setRequestHeader("Content-type", "application/json"); 
+     const formData = new FormData(form);
+
+    const object = {}
+    formData.forEach(function(value, key) {
+      object[key] = value;
+    })
+
+    const json = JSON.stringify(object)
+
+     request.send(json)
+
+     request.addEventListener('load' , () => {
+       if (request.status === 200) {
+         console.log(request.response);
+         statusMesage.textContent = message.success;
+         form.reset();
+         setTimeout(() => {
+           statusMesage.remove();
+         }, 2000)
+       } else {
+         statusMesage.textContent = message.failure;
+       }
+     })
+   });
+ }
 
 });
