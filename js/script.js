0b278ee5b85a2fb9e3d4bbe2076191f434fa1eca
CheateRYT <cheaterytyt@gmail.com>
2023-08-02 13:04:41 +0300

    Slider added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 window.addEventListener('DOMContentLoaded', function() {
 
     // Tabs
@@ -184,38 +182,12 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-
-    const getResource = async (url) => {
-        const res = await fetch(url);
-
-        if(!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`)
-        }
-
-    return await res.json()
-
-};
-getResource('http://localhost:3000/menu')
-.then(data => createCard(data));
-
-  function createCard(data) {
-    data.forEach(({img,altimg,title,descr,price}) => {
-        const element  = document.createElement('div')
-        element.classList.add('menu__item')
-
-        element.innerHTML = `
-        <img src=${img} alt=${altimg}>
-        <h3 class="menu__item-subtitle">${title}</h3>
-        <div class="menu__item-descr">${descr}</div>
-        <div class="menu__item-divider"></div>
-        <div class="menu__item-price">
-            <div class="menu__item-cost">Цена:</div>
-            <div class="menu__item-total"><span>${price}</span> грн/день</div>
-        </div>
-        `
-        document.querySelector('.menu .container').append(element);
-    })
-  }
+    getResource('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+            });
+        });
 
     // Forms
 
@@ -230,19 +202,27 @@ getResource('http://localhost:3000/menu')
         bindPostData(item);
     });
 
-
-    const postData = async (url,data) => {
-        const res = await fetch(url, {
-            method: 'POST',
+    const postData = async (url, data) => {
+        let res = await fetch(url, {
+            method: "POST",
             headers: {
                 'Content-Type': 'application/json'
             },
             body: data
-    })
+        });
+    
+        return await res.json();
+    };
 
-    return await res.json()
-
-};
+    async function getResource(url) {
+        let res = await fetch(url);
+    
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+    
+        return await res.json();
+    }
 
     function bindPostData(form) {
         form.addEventListener('submit', (e) => {
@@ -260,9 +240,7 @@ getResource('http://localhost:3000/menu')
 
             const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-
-
-            postData('http://localhost:3000/requests',json)
+            postData('http://localhost:3000/requests', json)
             .then(data => {
                 console.log(data);
                 showThanksModal(message.success);
@@ -298,7 +276,51 @@ getResource('http://localhost:3000/menu')
         }, 4000);
     }
 
+    // Slider
 
+    let slideIndex = 1;
+    const slides = document.querySelectorAll('.offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current');
 
+    showSlides(slideIndex);
 
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+
+        slides[slideIndex - 1].style.display = 'block'; 
+        
+        if (slides.length < 10) {
+            current.textContent =  `0${slideIndex}`;
+        } else {
+            current.textContent =  slideIndex;
+        }
+    }
+
+    function plusSlides (n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', function(){
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function(){
+        plusSlides(1);
+    });
 });
